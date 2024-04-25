f76e0b950c6f2ef8a6434b010ed7a5358fd8c3fe
zhunanyu <tiannuo90team@126.com>
2024-04-25 16:58:45 +0800

    ♻️refactor: 改为插件模式

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const params = process.argv.slice(2)
+const path = require("path");
 
-
-function run() {
+function run(params) {
   //TODO kesen: 2022-04-22  打开微信开发工具
   if (process.env.Path) {
-    let e = ''
-    process.env.Path.split(';').every((el) =>
-      el.includes('微信web开发者工具') ? (e = el) : true,
-    )
+    let e = "";
+    process.env.Path.split(";").every(el => (el.includes("微信web开发者工具") ? (e = el) : true));
 
-    e = e.replace(/(([A-Z]|[a-z])\:.*微信web开发者工具).*/g, '$1')
-    const weixin = '/d' + e
-    const { exec } = require('child_process')
+    e = e.replace(/(([A-Z]|[a-z])\:.*微信web开发者工具).*/g, "$1");
+    const weixin = "/d" + e;
+    const { exec } = require("child_process");
 
-    startWxIDE()
-    let time
-
+    startWxIDE();
+    let time;
 
     function startWxIDE() {
-      console.log('如无反应请检查开发工具安全->服务端口是否开启')
+      console.log("如无反应请检查开发工具安全->服务端口是否开启");
 
-      const self_env = { 'development': 'dev', 'production': 'build' }[process.env.NODE_ENV] || 'dev'
+      const self_env = { development: "dev", production: "build" }[process.env.NODE_ENV] || "dev";
       const self_project_url = path.resolve(
         process.cwd(),
-        params[0] || `./dist/${self_env}/mp-weixin`,
-      )
+        params || `./dist/${self_env}/mp-weixin`
+      );
 
-      exec(
-        `cd ${weixin} && cli open --project ${self_project_url}`,
-        (err) => {
-          if (err) {
-            console.log('启动失败，正在重新启动...', e)
-            console.error(err)
-            time = setTimeout(startWxIDE, 10000)
+      exec(`cd ${weixin} && cli open --project ${self_project_url}`, err => {
+        if (err) {
+          console.log("启动失败，正在重新启动...", e);
+          console.error(err);
+          time = setTimeout(startWxIDE, 10000);
 
-            return
-          }
-          console.log(`微信开发者工具启动成功!`, e)
-          clearInterval(time)
-        },
-      )
+          return;
+        }
+        console.log(`微信开发者工具启动成功! ${self_project_url}`);
+        clearInterval(time);
+      });
     }
   }
 }
 
+module.exports = (ctx, options) => {
+  ctx.onBuildStart(() => {});
+  ctx.onBuildFinish(() => {});
+  ctx.onBuildComplete(() => {
+    console.log("Taro 构建完成！");
+    run(options.path);
+  });
+};
 
-run()
